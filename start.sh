96032a307cca371cc791458e8a4ff3461d5caf8d
Gokul Krishna <gokris165@gmail.com>
2025-02-19 21:43:16 -0800

    v1.2, using pm2 to start backend and frontend with npm run prod script

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ touch .env.local
 echo "VITE_TOKEN=''" >> .env.local
 echo "VITE_BACKEND_HOST=''" >> .env.local
 echo "VITE_BACKEND_PORT=''" >> .env.local
-echo "VITE_FRONTEND_PORT=''" >> .env.local
+echo "VITE_FRONTEND_PORT=''" >> .env.local #also change this on pm2_config.json
